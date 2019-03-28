54cb7b21a719c0da896e104cba5b646eb9608920
Tushar <gc_tushar@yahoo.com>
2019-03-27 22:36:30 -0400

    Example added

--- genericpractice/src/main/java/client/Client.java
+++ genericpractice/src/main/java/client/Client.java
@@ -1,4 +1,29 @@
 package client;
 
+import generic.CompareExample;
+
 public class Client {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("\nExample with self define Compare: ");
+            System.out.println(CompareExample.compare("ab","ab"));
+            System.out.println(CompareExample.compare("ab","abc"));
+            System.out.println(CompareExample.compare(1.2,1.2));
+            System.out.println(CompareExample.compare(1,1));
+        }catch (NullPointerException e){
+            System.out.println("Exception In compare");
+        }
+
+
+        try {
+            System.out.println("\nExample with Compareable: ");
+            System.out.println(CompareExample.compareWithComparable("ab","ab"));
+            System.out.println(CompareExample.compareWithComparable("ab","abc"));
+            System.out.println(CompareExample.compareWithComparable(1.2,1.2));
+            System.out.println(CompareExample.compareWithComparable(1,1));
+        }catch (NullPointerException e){
+            System.out.println("Exception In compare");
+        }
+    }
 }
--- genericpractice/src/main/java/generic/CompareExample.java
+++ genericpractice/src/main/java/generic/CompareExample.java
@@ -24,28 +24,4 @@ public class CompareExample {
         System.out.print (ob1.toString() + " == " + ob2.toString() + " : ");
         return ob1.equals(ob2);
     }
-
-    public static void main(String[] args) {
-
-        try {
-            System.out.println("\nExample with self define Compare: ");
-            System.out.println(compareWithComparable("ab","ab"));
-            System.out.println(compareWithComparable("ab","abc"));
-            System.out.println(compareWithComparable(1.2,1.2));
-            System.out.println(compareWithComparable(1,1));
-        }catch (NullPointerException e){
-            System.out.println("Exception In compare");
-        }
-
-
-        try {
-            System.out.println("\nExample with Compareable: ");
-            System.out.println(compare("ab","ab"));
-            System.out.println(compare("ab","abc"));
-            System.out.println(compare(1.2,1.2));
-            System.out.println(compare(1,1));
-        }catch (NullPointerException e){
-            System.out.println("Exception In compare");
-        }
-    }
 }
