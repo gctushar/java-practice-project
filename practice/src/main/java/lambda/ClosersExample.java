061e5df00c7d4e5c9c2c8953c90e4a22d24b9e95
gctushar <gc_tushar@yahoo.com>
2022-06-26 01:31:18 -0400

    CloserExample Added

---
+++ practice/src/main/java/lambda/ClosersExample.java
@@ -0,0 +1,25 @@
+package lambda;
+
+public class ClosersExample {
+
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        doProcess(a, x -> System.out.println(x + b));
+//         b =5;
+    }
+
+    public static void doProcess(int a, Process process) {
+        process.process(a);
+    }
+
+
+    interface Process {
+        void process(int i);
+    }
+}
+
+
